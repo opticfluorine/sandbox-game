df2f31a52f5bdff697ffdc7379a37e1448960e45
opticfluorine <opticfluorine@gmail.com>
2021-10-11 02:30:46 +0000

    Add support for dev and prod builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,71 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LicensePlugin = require('webpack-license-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        hot: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: 'body',
-            showErrors: false,
-        }),
-        new ESLintPlugin({
-            extensions: ['ts'],
-            fix: true,
-        }),
-    ],
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000,
-    },
-    mode: 'development',
+module.exports = (env) => { 
+    // Environment configuration.
+    const isDev = (env.build === 'dev');
+
+    // Basic setup valid for dev or prod builds.
+    let exports = {
+        entry: './src/index.ts',
+        devtool: 'inline-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 
+                isDev ? 'dist-devel' : 'dist'),
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                inject: 'body',
+                showErrors: false,
+            }),
+            new ESLintPlugin({
+                extensions: ['ts'],
+                fix: true,
+            }),
+            new LicensePlugin({
+                unacceptableLicenseTest: (licenseId) => {
+                    return ['GPL', 'AGPL', 'LGPL', 'NGPL'].includes(licenseId);
+                },
+            }),
+            isDev && new webpack.HotModuleReplacementPlugin(),
+        ].filter(Boolean),
+        performance: {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000,
+        },
+        mode: isDev ? 'development' : 'production',
+    };
+
+    // Build specialization.
+    if (isDev) {
+        // Enable dev server with hot reloading.
+        exports.devServer = {
+            static: './dist-devel',
+            hot: true,
+        };
+    }
+
+    return exports;
 };
